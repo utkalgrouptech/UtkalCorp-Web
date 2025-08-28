5ebf67278c62a64e0292a85d59a65117af62644b
jigyanshu <nayakjigyanshu20@gmail.com>
2025-08-28 10:00:11 +0530

    Minor Changes

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function AboutPage() {
+export default function ProgramPage() {
   return (
     <main className="min-h-screen">
       {/* Hero Section */}
