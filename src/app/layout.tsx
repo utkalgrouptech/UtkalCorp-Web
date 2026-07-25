cc4296d20c21a9fc893d999c1ad057226e9d0dfb
utkalgrouptech <server@utkalsmart.com>
2026-07-25 12:45:45 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
-
 import "./globals.css";
+import Script from "next/script";
 import Footer from "@/layouts/Footer";
 import { Navbar } from "@/components/program/navbar";
-
 
 export const metadata = {
   title: "Utkal Corporation",
   icons: {
-    icon: "/favicon.ico", // or "/favicon.ico"
+    icon: "/favicon.ico",
   },
 };
-
 
 export default function RootLayout({
   children,
@@ -20,15 +18,29 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
-      <body
-        className=" " 
-        
-      >
-        <Navbar/>
+
+      <body className="">
+        <Navbar />
         {children}
-        <Footer/>
+        <Footer />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WG0NHBVF8P"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-WG0NHBVF8P');
+          `}
+        </Script>
       </body>
     </html>
   );
